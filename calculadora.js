d7bd0c1a3fab7d3f40c7da121bd02da8fc1dded1
josepineda221517 <140219202+josepineda221517@users.noreply.github.com>
2023-07-22 10:16:19 -0600

    initial commit

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+const primerNumero = document.getElementById('numero1');
+const signoop = document.getElementById('signo');
+const segundoNumero = document.getElementById('numero2');
+const primerboton = document.getElementById('boton');
+
+function calcular(){
+    const
+}
